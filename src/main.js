3022d5bc238c9310337ca56ae7e15fd079bd43ed
CodeSandbox <support@codesandbox.io>
2024-01-28 20:32:21 +0000

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,23 +1,74 @@
 import "./style.css";
-import javascriptLogo from "/javascript.svg";
-import { setupCounter } from "./counter.js";
+import * as THREE from "three";
+import gsap from "gsap";
+import hoverEffect from "hover-effect";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+let app = document.querySelector("#app");
+let container = document.createElement("div");
+container.id = "container";
+app.appendChild(container);
 
-setupCounter(document.querySelector("#counter"));
+var myAnimation1 = new hoverEffect({
+  parent: document.querySelector("#container"),
+  intensity1: 0.1,
+  intensity2: 0.1,
+  angle2: Math.PI / 2,
+  image1:
+    "https://upload.wikimedia.org/wikipedia/commons/3/30/Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg",
+  image2:
+    "https://images.unsplash.com/photo-1440330033336-7dcff4630cef?ixlib=rb-1.2.1&auto=format&fit=crop&w=1342&q=80",
+  displacementImage:
+    "https://cdn.rawgit.com/robin-dela/hover-effect/b6c6fd26/images/stripe1mul.png?raw=true",
+});
+
+var myAnimation2 = new hoverEffect({
+  parent: document.querySelector("#container"),
+  intensity1: 0.1,
+  intensity2: 0.1,
+  angle2: Math.PI / 2,
+  image1:
+    "https://upload.wikimedia.org/wikipedia/commons/3/30/Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg",
+  image2:
+    "https://images.unsplash.com/photo-1440330033336-7dcff4630cef?ixlib=rb-1.2.1&auto=format&fit=crop&w=1342&q=80",
+  displacementImage:
+    "https://cdn.rawgit.com/robin-dela/hover-effect/b6c6fd26/images/stripe1mul.png?raw=true",
+});
+
+var myAnimation3 = new hoverEffect({
+  parent: document.querySelector("#container"),
+  intensity1: 0.1,
+  intensity2: 0.1,
+  angle2: Math.PI / 2,
+  image1:
+    "https://upload.wikimedia.org/wikipedia/commons/3/30/Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg",
+  image2:
+    "https://images.unsplash.com/photo-1440330033336-7dcff4630cef?ixlib=rb-1.2.1&auto=format&fit=crop&w=1342&q=80",
+  displacementImage:
+    "https://cdn.rawgit.com/robin-dela/hover-effect/b6c6fd26/images/stripe1mul.png?raw=true",
+});
+
+var myAnimation4 = new hoverEffect({
+  parent: document.querySelector("#container"),
+  intensity1: 0.1,
+  intensity2: 0.1,
+  angle2: Math.PI / 2,
+  image1:
+    "https://upload.wikimedia.org/wikipedia/commons/3/30/Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg",
+  image2:
+    "https://images.unsplash.com/photo-1440330033336-7dcff4630cef?ixlib=rb-1.2.1&auto=format&fit=crop&w=1342&q=80",
+  displacementImage:
+    "https://cdn.rawgit.com/robin-dela/hover-effect/b6c6fd26/images/stripe1mul.png?raw=true",
+});
+
+var myAnimation5 = new hoverEffect({
+  parent: document.querySelector("#container"),
+  intensity1: 0.1,
+  intensity2: 0.1,
+  angle2: Math.PI / 2,
+  image1:
+    "https://upload.wikimedia.org/wikipedia/commons/3/30/Echo_Park_Lake_with_Downtown_Los_Angeles_Skyline.jpg",
+  image2:
+    "https://images.unsplash.com/photo-1440330033336-7dcff4630cef?ixlib=rb-1.2.1&auto=format&fit=crop&w=1342&q=80",
+  displacementImage:
+    "https://cdn.rawgit.com/robin-dela/hover-effect/b6c6fd26/images/stripe1mul.png?raw=true",
+});
